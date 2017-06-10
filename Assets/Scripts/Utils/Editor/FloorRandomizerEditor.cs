139e4196d6903abc5dd8a0731d9be849aeb14628
Nazarov Andrey <nazarov.andrey@me.com>
2017-06-10 22:07:12 +0300

    floor randomizer

---
+++ Assets/Scripts/Utils/Editor/FloorRandomizerEditor.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine.UI;
+
+namespace Dust.Utils {
+	[CustomEditor (typeof (FloorRandomizer))]
+	public class FloorRandomizerEditor : Editor
+	{
+		private SerializedProperty sprites;
+		private ReorderableList reorderableList;
+
+		private void OnEnable ()
+		{
+			sprites = serializedObject.FindProperty ("sprites");
+			reorderableList = new ReorderableList (serializedObject, sprites);
+			reorderableList.drawElementCallback = DrawElementCallback;
+			reorderableList.drawHeaderCallback = DrawHeaderCallback;
+		}
+
+		private void DrawHeaderCallback (Rect position)
+		{
+			EditorGUI.LabelField (position, "Sprites");
+		}
+
+		private void DrawElementCallback (Rect position, int index, bool isActive, bool isFocused)
+		{
+			SerializedProperty element = sprites.GetArrayElementAtIndex (index);
+			position.y += 2;
+			position.height -= 5;
+			EditorGUI.PropertyField (position, element, GUIContent.none); 
+		}
+
+		public override void OnInspectorGUI ()
+		{
+			serializedObject.Update ();
+			reorderableList.DoLayoutList ();
+
+			Rect position = EditorGUILayout.GetControlRect ();
+			position.width = 80;
+			if (GUI.Button (position, "Rebuild")) {
+				if (sprites.arraySize == 0)
+					throw new System.InvalidOperationException ("Specify at least one sprite");
+
+				FloorRandomizer floorRandomizer = serializedObject.targetObject as FloorRandomizer;
+				Image[] images = floorRandomizer.GetComponentsInChildren<Image> ();
+				foreach (var image in images) {
+					Sprite sprite = sprites.GetArrayElementAtIndex (
+						Random.Range (0, sprites.arraySize)).objectReferenceValue as Sprite;
+
+					SerializedObject serializedObject = new SerializedObject (image);
+					SerializedProperty serializedProperty = serializedObject.FindProperty ("m_Sprite");
+					serializedProperty.objectReferenceValue = sprite;
+					serializedObject.ApplyModifiedProperties ();
+				}
+			}
+				
+
+			serializedObject.ApplyModifiedProperties ();
+		}
+	}
+}
---
+++ Assets/Scripts/Utils/FloorRandomizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dust.Utils {
+	[ExecuteInEditMode]
+	public class FloorRandomizer : MonoBehaviour
+	{
+		[SerializeField]
+		private List<Sprite> sprites;
+
+		public void Rebuild ()
+		{
+
+		}
+	}
+}
